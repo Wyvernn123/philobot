1688bdc4823c6144f1d989ea32c9c1f1ea24b293
alexandre bristiel <wyvernn123@gmail.com>
2019-12-04 15:51:19 +0100

    a

--- philogod.py
+++ philogod.py
@@ -3,11 +3,16 @@
 Created on Wed Dec  4 00:25:20 2019
 
 @author: alexandre
-"""
+"""import os
 
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
 from discord.ext import commands
-import random 
+import random
 
 Parole_de_Dieu = [f'Un trou, un manque, une absence, n’étant rien, imposent qu’on tourne autour, c’est-à-dire ce minimum de danse qui est déjà un style.',f'Le « petit a » par exemple, un peu comme le « ptyx » de Mallarmé, espèce de petit x universel, conviendra d’autant mieux qu’il n’existe pas dans la langue naturelle.',f'Le minimum alors poussé au maximum des figures dessinera plus nettement la chose qui le suscite, comme c’est l’énormité bourdonnante de l’essaim qui conduit l’oeil au faible pédoncule auquel il se trouve appendu.',f'La Musique sans les Lettres se présente comme très subtil nuage : seules, elles, une monnaie si courante.']
 
@@ -24,4 +29,4 @@ async def illumination(ctx):
     await ctx.send(emote(14))
 
 
-client.run('NjUxNTUzMjU2MDEwNDE2MTQw.XefFmw.YGvMKu6t5ITIstMwFP3kYlW7wlY')
+client.run(token)
