bee4ffc8a3febf2d020782e15e7e1a2f962b6a7f
alexandre bristiel <wyvernn123@gmail.com>
2019-12-06 20:11:44 +0100

    Update

--- philogod.py
+++ philogod.py
@@ -9,7 +9,11 @@ import os
 from discord.ext import commands
 import numpy
 
-Parole_de_Dieu = [f'Un trou, un manque, une absence, n’étant rien, imposent qu’on tourne autour, c’est-à-dire ce minimum de danse qui est déjà un style.',f'Le « petit a » par exemple, un peu comme le « ptyx » de Mallarmé, espèce de petit x universel, conviendra d’autant mieux qu’il n’existe pas dans la langue naturelle.',f'Le minimum alors poussé au maximum des figures dessinera plus nettement la chose qui le suscite, comme c’est l’énormité bourdonnante de l’essaim qui conduit l’oeil au faible pédoncule auquel il se trouve appendu.',f'La Musique sans les Lettres se présente comme très subtil nuage : seules, elles, une monnaie si courante.']
+Sagesse_infini = open("Parole de Dieu.txt",'r')
+Parole_de_Dieu = []
+for Infime_partie in Sagesse_infini:
+    Citation = Infime_partie.strip()
+    Parole_de_Dieu.append(Citation)
 
 client = commands.Bot(command_prefix = '>')
 def emote(id):
